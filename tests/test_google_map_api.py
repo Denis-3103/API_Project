968cf4db2d32b3dd5a22d80370b1f784a2b9225e
Denis Matveichuk <matveichuk.dn@gmail.com>
2022-11-27 17:58:27 +0300

    init post

---
+++ tests/test_google_map_api.py
@@ -0,0 +1,12 @@
+from utils.api import Google_maps_api
+from requests import Response
+
+"""Создание, изменение и удаление новой локации"""
+
+class Test_create_place():
+
+
+    def test_create_new_place(self):
+
+        print("Метод POST")
+        result_post: Response = Google_maps_api.create_new_place()
